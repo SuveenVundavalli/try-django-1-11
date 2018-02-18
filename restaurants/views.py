8b0194ad221ee6632f59aa4eb4c2da0c50628802
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-02-18 20:12:27 +0530

    added first model RestaurantLocation

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
 import random
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 
 # Create your views here.
@@ -15,7 +16,6 @@ def home(request):
 
 
 def about(request):
-
     return render(request, 'about.html')  # response
 
 
@@ -23,7 +23,14 @@ def contact(request):
     return render(request, 'contact.html')  # response
 
 
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, 'contact.html', context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # Getting predefined context from TemplateView
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = random.randint(0, 10000000000)
+        some_list = [random.randint(0, 10000000000), random.randint(0, 10000000000), random.randint(0, 10000000000)]
+        context = {'html_var': True, 'num': num, 'some_list': some_list}
+        return context
+
