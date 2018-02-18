8cca1aa76dae133d4488a92286d86ed6cd19abbb
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-02-18 21:27:53 +0530

    added Q lookups and slug fields

--- cfe/urls.py
+++ cfe/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    RestaurantListView,
+    SearchRestaurantListView,
+)
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
-    url(r'^restaurants/$', restaurant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)/$', SearchRestaurantListView.as_view()),
+    # url(r'^restaurants/north/$', NorthIndianRestaurantListView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import RestaurantLocation
+from django.views.generic import ListView
+from django.db.models import Q
 
 
 # Create your views here.
@@ -9,3 +11,26 @@ def restaurant_listview(request):
     queryset = RestaurantLocation.objects.all()
     context = {'object_list': queryset}
     return render(request, template_name, context)
+
+
+class RestaurantListView(ListView):
+    template_name = 'restaurants/restaurant_list.html'
+    queryset = RestaurantLocation.objects.all()
+
+
+class SearchRestaurantListView(ListView):
+    template_name = 'restaurants/restaurant_list.html'
+
+    def get_queryset(self):
+        print('in slug')
+        print(self.kwargs)
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__icontains=slug) |
+                Q(name__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.none()
+        return queryset
+
